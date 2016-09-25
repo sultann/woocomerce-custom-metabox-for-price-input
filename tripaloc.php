8deea5ed37ef07f560d6f171a304f36780be3b89
sultann <manikdrmc@gmail.com>
2016-09-25 20:50:02 +0600

    initial

---
+++ tripaloc.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Tripaloc
+Plugin URI: http://www.wpfeature.com
+Description: Custom Plugin
+Version: 1.0
+Author: Sultan Nasir Uddin
+Author URI: http://imanik.me
+*/
+if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+require_once plugin_dir_path(__FILE__).'/cs-framework/cs-framework.php';
+
